586fb5ce34978515918b0c30dccab0187062d3f5
Hafiz Muhammad Abdullah <hm.abdullah@2bvision.com>
2025-07-08 20:00:33 +0500

    profile image

--- screens/tabNavigation/components/ProfileImage.jsx
+++ screens/tabNavigation/components/ProfileImage.jsx
@@ -1,21 +1,47 @@
-import { StyleSheet, Text, View,Image } from 'react-native'
-import React from 'react'
-import { useSelector } from 'react-redux'
- import AsyncStorage from '@react-native-async-storage/async-storage'
+import { StyleSheet, Text, View, Image } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { MMKV } from 'react-native-mmkv';
 
+// Create MMKV instance
+const storage = new MMKV();
 
 const ProfileImage = () => {
+  const user = useSelector(state => state.auth); // Get from Redux
+  const profileImageFromRedux = user?.data?.data?.profileImage;
 
-const user = useSelector((state) => state.auth.user.data);
+  const [profileImage, setProfileImage] = useState<string | null>(null);
+
+  // ✅ Store to MMKV (only when Redux has value)
+  useEffect(() => {
+    if (profileImageFromRedux) {
+      storage.set('profileImage', profileImageFromRedux); // Save to MMKV
+    }
+    // Always get from MMKV
+    const imageFromStorage = storage.getString('profileImage');
+    setProfileImage(imageFromStorage ?? null);
+  }, [profileImageFromRedux]);
 
   return (
-    <View>
-      <Image source={{uri:user?.data?.data?.profileImage}} style={{width:50,height:50,borderRadius:50}}/>
+    <View style={styles.container}>
+      {profileImage ? (
+        <Image
+          source={{ uri: profileImage }}
+          style={{ width: 50, height: 50, borderRadius: 25 }}
+        />
+      ) : (
+        <Text>No Image Found</Text>
+      )}
       <Text>ProfileImage</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ProfileImage
+export default ProfileImage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+});
